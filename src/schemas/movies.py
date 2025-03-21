b70c181b4262fda59a8e1da8d15386053c95f918
Anton Konyk <antonkonyk.com>
2025-03-21 19:56:45 +0100

    create MovieBase

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,20 @@
 # Write your code here
+import datetime
+
+from pydantic import BaseModel
+
+
+class MovieBase(BaseModel):
+    name: str
+    date: datetime.date
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+
